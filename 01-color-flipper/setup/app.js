b86a24724282efba4c6688e1ed947cbf8d4714dd
thelaserunicorn <rashideram2@gmail.com>
2021-09-21 15:03:05 +0600

    color-picker

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+function getRandomColor(){
+    return Math.floor(Math.random() * colors.length);
+
+}
+
+btn.addEventListener("click", function(){
+    const randomNumber = getRandomColor();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+    color.style.color = colors[randomNumber];
+})
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,28 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+
+function getRandomColor(){
+    return Math.floor(Math.random() * hex.length);
+
+}
+
+btn.addEventListener("click", function(){
+    let hexColor = "#";
+    for(i=0; i<6;i++){
+        hexColor += hex[getRandomColor()]
+    }
+
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
+    console.log(hexColor);
+
+    if(hexColor == "#00000" ){
+        hexColor = "#FFFFFF";
+        color.style.color = hexColor
+    }
+    color.style.color = hexColor;
+})
+
